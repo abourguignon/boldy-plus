e8474de3468cff17e5c29751778e7f8205acb0af
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2013-06-19 17:12:52 +0200

    Files check: update the doctype (to html5) and indentation of header.php

--- header.php
+++ header.php
@@ -1,53 +1,56 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta name="keywords" content="<?php echo get_option('boldy_keywords'); ?>" />
-<meta name="description" content="<?php echo get_option('boldy_description'); ?>" />
-<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" media="all" />
-<link href="<?php bloginfo('template_directory'); ?>/css/ddsmoothmenu.css" rel="stylesheet" type="text/css" />
-<link href="<?php bloginfo('template_directory'); ?>/css/prettyPhoto.css" rel="stylesheet" type="text/css" />
-<link href="<?php bloginfo('template_directory'); ?>/css/nivo-slider.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery-1.7.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.form.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/ddsmoothmenu.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.nivo.slider.pack.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.prettyPhoto.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/custom.js"></script>
-<?php if (get_option('boldy_cufon') != "no") : ?>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/cufon-yui.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/Museo_Slab_500_400.font.js"></script>
-<script type="text/javascript">
-    Cufon.replace('h1',{hover: true})('h2',{hover: true})('h3')('.reply',{hover:true})('.more-link');
-</script>
-<?php endif; ?>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#quickContactForm').ajaxForm(function(data){
-            if (data==1)
-            {
-                $('#success').fadeIn("slow");
-                $('#bademail').fadeOut("slow");
-                $('#badserver').fadeOut("slow");
-                $('#contact').resetForm();
-            }
-            else if (data==2)
-            {
-                $('#badserver').fadeIn("slow");
-            }
-            else if (data==3)
-            {
-                $('#bademail').fadeIn("slow");
-            }
-        });
-    });
-</script>
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta name="keywords" content="<?php echo get_option('boldy_keywords'); ?>" />
+    <meta name="description" content="<?php echo get_option('boldy_description'); ?>" />
 
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?php bloginfo('template_directory'); ?>/css/ddsmoothmenu.css" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo('template_directory'); ?>/css/prettyPhoto.css" rel="stylesheet" type="text/css" />
+    <link href="<?php bloginfo('template_directory'); ?>/css/nivo-slider.css" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery-1.7.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.form.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/ddsmoothmenu.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.nivo.slider.pack.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/custom.js"></script>
+    <?php if (get_option('boldy_cufon') != "no") : ?>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/cufon-yui.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/Museo_Slab_500_400.font.js"></script>
+    <script type="text/javascript">
+        Cufon.replace('h1',{hover: true})('h2',{hover: true})('h3')('.reply',{hover: true})('.more-link');
+    </script>
+    <?php endif; ?>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#quickContactForm').ajaxForm(function(data){
+                if (data==1)
+                {
+                    $('#success').fadeIn("slow");
+                    $('#bademail').fadeOut("slow");
+                    $('#badserver').fadeOut("slow");
+                    $('#contact').resetForm();
+                }
+                else if (data==2)
+                {
+                    $('#badserver').fadeIn("slow");
+                }
+                else if (data==3)
+                {
+                    $('#bademail').fadeIn("slow");
+                }
+            });
+        });
+    </script>
+
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php wp_get_archives('type=monthly&format=link'); ?>
     <?php //comments_popup_script(); // off by default ?>
-    <?php wp_head(); ?>
+    <?php wp_head(); //plugins use this action hook to add their own scripts, stylesheets, and other functionality ?>
 </head>
 
 <body <?php if(is_home())
@@ -71,9 +74,9 @@
                 <a href="<?php bloginfo('url'); ?>/"><img src="<?php echo bloginfo('template_directory');?>/images/logo.png" alt="<?php echo get_option('boldy_logo_alt'); ?>" /></a>
                 <?php endif; ?>
             </div>
-            
+
             <!-- BEGIN MAIN MENU -->
-            <?php 
+            <?php
             if (function_exists('wp_nav_menu')) {
                 wp_nav_menu( array('theme_location' => 'main-menu', 'container_id' => 'mainMenu', 'container_class' => 'ddsmoothmenu', 'fallback_cb'=>'primarymenu'));
             }
@@ -104,10 +107,10 @@
                     <?php }?>
                     <li><a href="<?php bloginfo('rss2_url'); ?>" title="RSS" class="rss"><img src="<?php bloginfo('template_directory'); ?>/images/ico_rss.png" alt="Subcribe to Our RSS Feed" /></a></li>
                 </ul>
-            </div>    
+            </div>
             <!-- END TOP SOCIAL LINKS -->
         </div>
         <!-- END HEADER -->
-        
+
         <!-- BEGIN CONTENT -->
         <div id="content">
