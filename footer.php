b1f47ba91d47c7cb2d536526cf2f381cb1a54c4b
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2013-06-19 17:13:11 +0200

    Files check: update indentation of footer.php

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
- </div>
-           <!-- END CONTENT -->
+</div>
+<!-- END CONTENT -->
     </div>
     <!-- END WRAPPER -->
-    
+
     <!-- BEGIN FOOTER -->
     <div id="footer">
-    <?php if(get_option('boldy_footer_actions')!="no") {?>
+        <?php if (get_option('boldy_footer_actions')!="no"): ?>
         <div style="width:960px; margin: 0 auto; position:relative;">
             <a href="#" id="showHide" <?php if(get_option('boldy_actions_hide')=="hidden"){echo 'style="background-position:0 -16px"';}?>>
                 <?php _e("Show/Hide footer actions", "boldy-plus"); ?>
             </a>
         </div>
-        
+
         <div id="footerActions" <?php if(get_option('boldy_actions_hide')=="hidden"){echo 'style="display:none"';}?>>
             <div id="footerActionsInner">
-            <?php if(get_option('boldy_twitter_user')!="" && get_option('boldy_latest_tweet')!="no"){ ?>
+                <?php if (get_option('boldy_twitter_user')!="" && get_option('boldy_latest_tweet')!="no"): ?>
                 <div id="twitter">
                     <a href="http://twitter.com/<?php echo get_option('boldy_twitter_user'); ?>" class="action">
                         <?php _e("Follow us!", "boldy-plus"); ?>
@@ -26,40 +26,38 @@
                         <div id="tweetBottom"></div>
                     </div>
                 </div>
-                <?php } ?>                    
+                <?php endif; ?>
                 <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
-                <script type="text/javascript" src="http://twitter.com/statuses/user_timeline/<?php echo get_option('boldy_twitter_user'); ?>.json?callback=twitterCallback2&amp;count=<?php 
-                if(get_option('boldy_number_tweets')!=""){
-                    echo get_option('boldy_number_tweets');
-                    }else{
-                        echo "1";
-                    } ?>">
+                <script
+                    type="text/javascript"
+                    src="http://twitter.com/statuses/user_timeline/<?php echo get_option('boldy_twitter_user'); ?>.json?callback=twitterCallback2&amp;count=<?php echo (get_option('boldy_number_tweets')!="" ? get_option('boldy_number_tweets') : "1"); ?>">
                 </script>
                 <div id="quickContact">
                     <p id="success" class="successmsg" style="display:none;"><?php _e("Your e-mail has been sent. Thank you!", "boldy-plus"); ?></p>
                     <p id="bademail" class="errormsg" style="display:none;"><?php _e("Please enter your name, a message and a valid e-mail address.", "boldy-plus"); ?></p>
                     <p id="badserver" class="errormsg" style="display:none;"><?php _e("Your e-mail failed. Try again later.", "boldy-plus"); ?></p>
-                    
+
                     <form action="<?php bloginfo('template_url'); ?>/sendmail.php" method="post" id="quickContactForm">
-                    <div class="leftSide">
-                        <input type="text" value="<?php _e("Your name", "boldy-plus"); ?>" id="quickName" name="name" />
-                        <input type="text" value="<?php _e("Your e-mail", "boldy-plus"); ?>" id="quickEmail" name="email" />
-                        <input type="submit" name="submit" id="submitinput" value="<?php _e("Send", "boldy-plus"); ?>"/>
-                    </div>
-                    <div class="rightSide">
-                        <textarea id="quickComment" name="comment"><?php _e("Your message", "boldy-plus"); ?></textarea>
-                    </div>
-                    <input type="hidden" id="quickReceiver" name="receiver" value="<?php echo strhex(get_option('boldy_contact_email')); ?>"/>
+                        <div class="leftSide">
+                            <input type="text" value="<?php _e("Your name", "boldy-plus"); ?>" id="quickName" name="name" />
+                            <input type="text" value="<?php _e("Your e-mail", "boldy-plus"); ?>" id="quickEmail" name="email" />
+                            <input type="submit" name="submit" id="submitinput" value="<?php _e("Send", "boldy-plus"); ?>"/>
+                        </div>
+                        <div class="rightSide">
+                            <textarea id="quickComment" name="comment"><?php _e("Your message", "boldy-plus"); ?></textarea>
+                        </div>
+                        <input type="hidden" id="quickReceiver" name="receiver" value="<?php echo strhex(get_option('boldy_contact_email')); ?>"/>
                     </form>
                 </div>
             </div>
         </div>
-        <?php }?>
+        <?php endif; ?>
+
         <div id="footerWidgets">
             <div id="footerWidgetsInner">
                 <!-- BEGIN FOOTER WIDGET -->
                 <?php /* Widgetized sidebar */
-                if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer') ) : ?><?php endif; ?>
+                if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('footer')): ?><?php endif; ?>
                 <!-- END FOOTER WIDGETS -->
                 <!-- BEGIN COPYRIGHT -->
                 <div id="copyright">
@@ -72,18 +70,18 @@
                         {
                             _e("Just go to Theme options page and edit copyright text", "boldy-plus");
                         }
-                    ?> 
+                    ?>
                     <!-- Pretty theme made in Site5: http://www.site5.com/ -->
                 </div>
-                <!-- END COPYRIGHT -->                        
-                </div>
-                
+                <!-- END COPYRIGHT -->
+            </div>
+
         </div>
-    </div>    
+    </div>
     <!-- END FOOTER -->
 </div>
 <!-- END MAINWRAPPER -->
-<?php if (get_option(' boldy_analytics') <> "") : ?>
+<?php if (get_option(' boldy_analytics') <> ""): ?>
 <?php echo stripslashes(stripslashes(get_option('boldy_analytics'))); ?>
 <?php endif; ?>
 
