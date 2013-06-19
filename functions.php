55ad9c1381182d6dab8907cb5f024467a24bb843
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2013-06-19 17:47:41 +0200

    Fix a syntax error in functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 ********************************/
 
 add_action('after_setup_theme', 'boldy_i18n');
-        
+
 function boldy_i18n() {
     load_theme_textdomain('boldy-plus', get_template_directory() . '/languages');
 }
@@ -17,33 +17,33 @@ function boldy_i18n() {
 
 if ( function_exists( 'wp_nav_menu' ) )
 {
-    if (function_exists('add_theme_support')) 
+    if (function_exists('add_theme_support'))
     {
         add_theme_support('nav-menus');
         add_action('init', 'register_my_menus');
-        
+
         function register_my_menus() {
             register_nav_menus(array('main-menu' => __( 'Main menu' )));
         }
     }
 }
 
-/* 
- * CallBack functions for menus in case of earlier than 3.0 Wordpress version 
+/*
+ * CallBack functions for menus in case of earlier than 3.0 Wordpress version
  * or if no menu is set yet
  */
-function primarymenu() { 
+function primarymenu() {
     ?>
     <div id="mainMenu" class="ddsmoothmenu">
         <ul>
-            <?php 
+            <?php
             wp_list_categories(
-                'hide_empty=1&title_li=<a href="#">'.__('News').'</a>'); 
-            wp_list_pages('title_li='); 
+                'hide_empty=1&title_li=<a href="#">'.__('News').'</a>');
+            wp_list_pages('title_li=');
             ?>
         </ul>
     </div>
-    <?php 
+    <?php
 }
 
 
@@ -52,7 +52,7 @@ function primarymenu() {
 ********************************/
 
 /**
- * Fetch or display a list of pages public AND private as a dropdown 
+ * Fetch or display a list of pages public AND private as a dropdown
  * (select list).
  */
 function boldy_dropdown_pages($args = '') {
@@ -63,21 +63,21 @@ function boldy_dropdown_pages($args = '') {
         'show_option_none' => '', 'show_option_no_change' => '',
         'option_none_value' => ''
     );
-    
+
     $r = wp_parse_args( $args, $defaults );
     extract( $r, EXTR_SKIP );
-    
+
     // Super workaround to display private pages trololol
     $pages_publish = get_pages(array_merge($r, array('post_status' => 'publish')));
     $pages_private = get_pages(array_merge($r, array('post_status' => 'private')));
     $pages = array_merge($pages_publish, $pages_private);
-    
+
     $output = '';
     $name = esc_attr($name);
     // Back-compat with old system where both id and name were based on $name argument
     if ( empty($id) )
         $id = $name;
-    
+
     if ( ! empty($pages) ) {
         $output = "<select name=\"$name\" id=\"$id\">\n";
         if ( $show_option_no_change )
@@ -87,12 +87,12 @@ function boldy_dropdown_pages($args = '') {
         $output .= walk_page_dropdown_tree($pages, $depth, $r);
         $output .= "</select>\n";
     }
-    
+
     $output = apply_filters('wp_dropdown_pages', $output);
-    
+
     if ( $echo )
         echo $output;
-    
+
     return $output;
 }
 
@@ -104,13 +104,14 @@ function boldy_dropdown_pages($args = '') {
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 300, 200, true );
 
-/* Get the thumb original image full url */
-
-function get_thumb_urlfull ($postID) {
-$image_id = get_post_thumbnail_id($post);  
-$image_url = wp_get_attachment_image_src($image_id,'large');  
-$image_url = $image_url[0]; 
-return $image_url;
+/**
+ * Get the thumb original image full url
+ */
+function get_thumb_urlfull($post) {
+    $image_id = get_post_thumbnail_id($post->ID);
+    $image_url = wp_get_attachment_image_src($image_id,'large');
+    $image_url = $image_url[0];
+    return $image_url;
 }
 
 
@@ -158,19 +159,19 @@ register_sidebar(array(
  *      automatically determine the value.
  * 'range' - Default is 3 (int). The number of page links to show before and after
  *      the current page.
- * 'gap' - Default is 3 (int). The minimum number of pages before a gap is 
+ * 'gap' - Default is 3 (int). The minimum number of pages before a gap is
  *      replaced with ellipses (...).
  * 'anchor' - Default is 1 (int). The number of links to always show at begining
  *      and end of pagination
- * 'before' - Default is '<div class="emm-paginate">' (string). The html or text 
+ * 'before' - Default is '<div class="emm-paginate">' (string). The html or text
  *      to add before the pagination links.
  * 'after' - Default is '</div>' (string). The html or text to add after the
  *      pagination links.
  * 'title' - Default is '__('Pages').':'' (string). The text to display before the
  *      pagination links.
- * 'next_page' - Default is '&raquo;' (string). The text to use for the 
+ * 'next_page' - Default is '&raquo;' (string). The text to use for the
  *      next page link.
- * 'previous_page' - Default is '&laquo' (string). The text to use for the 
+ * 'previous_page' - Default is '&laquo' (string). The text to use for the
  *      previous page link.
  * 'echo' - Default is 1 (int). To return the code instead of echo'ing, set this
  *      to 0 (zero).
@@ -182,10 +183,10 @@ register_sidebar(array(
  * @param array|string $args Optional. Override default arguments.
  * @return string HTML content, if not displaying.
  */
- 
+
 function emm_paginate($args = null) {
     $defaults = array(
-        'page' => null, 'pages' => null, 
+        'page' => null, 'pages' => null,
         'range' => 3, 'gap' => 3, 'anchor' => 1,
         'before' => '<div class="emm-paginate">', 'after' => '</div>',
         'title' => __('Pages').':',
@@ -205,16 +206,16 @@ function emm_paginate($args = null) {
         $posts_per_page = intval(get_query_var('posts_per_page'));
         $pages = intval(ceil($wp_query->found_posts / $posts_per_page));
     }
-    
+
     $output = "";
-    if ($pages > 1) {    
+    if ($pages > 1) {
         $output .= "$before<span class='emm-title'>$title</span>";
         $ellipsis = "<span class='emm-gap'>...</span>";
 
         if ($page > 1 && !empty($previouspage)) {
             $output .= "<a href='" . get_pagenum_link($page - 1) . "' class='emm-prev'>$previouspage</a>";
         }
-        
+
         $min_links = $range * 2 + 1;
         $block_min = min($page - $range, $pages - $min_links);
         $block_high = max($page + $range, $min_links);
@@ -222,23 +223,23 @@ function emm_paginate($args = null) {
         $right_gap = (($block_high + $anchor + $gap) < $pages) ? true : false;
 
         if ($left_gap && !$right_gap) {
-            $output .= sprintf('%s%s%s', 
-                emm_paginate_loop(1, $anchor), 
-                $ellipsis, 
+            $output .= sprintf('%s%s%s',
+                emm_paginate_loop(1, $anchor),
+                $ellipsis,
                 emm_paginate_loop($block_min, $pages, $page)
             );
         }
         else if ($left_gap && $right_gap) {
-            $output .= sprintf('%s%s%s%s%s', 
-                emm_paginate_loop(1, $anchor), 
-                $ellipsis, 
-                emm_paginate_loop($block_min, $block_high, $page), 
-                $ellipsis, 
+            $output .= sprintf('%s%s%s%s%s',
+                emm_paginate_loop(1, $anchor),
+                $ellipsis,
+                emm_paginate_loop($block_min, $block_high, $page),
+                $ellipsis,
                 emm_paginate_loop(($pages - $anchor + 1), $pages)
             );
         }
         else if ($right_gap && !$left_gap) {
-            $output .= sprintf('%s%s%s', 
+            $output .= sprintf('%s%s%s',
                 emm_paginate_loop(1, $block_high, $page),
                 $ellipsis,
                 emm_paginate_loop(($pages - $anchor + 1), $pages)
@@ -278,8 +279,8 @@ function emm_paginate($args = null) {
 function emm_paginate_loop($start, $max, $page = 0) {
     $output = "";
     for ($i = $start; $i <= $max; $i++) {
-        $output .= ($page === intval($i)) 
-            ? "<span class='emm-page emm-current'>$i</span>" 
+        $output .= ($page === intval($i))
+            ? "<span class='emm-page emm-current'>$i</span>"
             : "<a href='" . get_pagenum_link($i) . "' class='emm-page'>$i</a>";
     }
     return $output;
@@ -302,7 +303,7 @@ function post_is_in_descendant_category( $cats, $_post = null )
 ********************************/
 
 function mytheme_comment($comment, $args, $depth) {
-    $GLOBALS['comment'] = $comment; 
+    $GLOBALS['comment'] = $comment;
     ?>
     <li <?php comment_class('clearfix'); ?> id="li-comment-<?php comment_ID() ?>">
         <div class="gravatar">
@@ -311,26 +312,26 @@ function mytheme_comment($comment, $args, $depth) {
         </div>
         <div id="comment-<?php comment_ID(); ?>">
             <div class="comment-meta commentmetadata clearfix">
-                <strong><?php printf('%s', get_comment_author_link()) ?></strong> 
-                (<?php edit_comment_link(__('edit')) ?>) 
+                <strong><?php printf('%s', get_comment_author_link()) ?></strong>
+                (<?php edit_comment_link(__('edit')) ?>)
                 <span>
                     <?php printf(__('%1$s at %2$s'), get_comment_date(),  get_comment_time('G\hi')); ?>
                 </span>
             </div>
-            
+
             <div class="text">
                 <?php comment_text() ?>
             </div>
-            
+
             <?php if ($comment->comment_approved == '0') : ?>
             <em><?php _e('Your comment is awaiting moderation.') ?></em><br />
             <?php endif; ?>
-            
+
             <div class="reply">
             <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
             </div>
         </div>
-<?php 
+<?php
 }
 
 
@@ -344,7 +345,7 @@ function boldy_theme_page ()
     if ( count($_POST) > 0 && isset($_POST['boldy_settings']) )
     {
         $options = array (
-            'logo_img', 
+            'logo_img',
             'logo_alt',
             'contact_email',
             'contact_text',
@@ -374,13 +375,13 @@ function boldy_theme_page ()
             'portfolio',
             'blog',
             'slider');
-        
+
         foreach ( $options as $opt )
         {
             delete_option ( 'boldy_'.$opt, $_POST[$opt] );
             add_option ( 'boldy_'.$opt, $_POST[$opt] );
         }
-        
+
     }
     add_menu_page(__('Boldy options'), __('Boldy options'), 'edit_themes', basename(__FILE__), 'boldy_settings');
     add_submenu_page(__('Boldy options'), __('Boldy options'), 'edit_themes', basename(__FILE__), 'boldy_settings');
@@ -393,12 +394,12 @@ function boldy_settings()
     h4 {
         margin: 0;
     }
-    
+
     legend {
         margin-left: 5px;
         padding: 0 5px;
     }
-    
+
     p.notice {
         color: #666;
     }
@@ -406,22 +407,22 @@ function boldy_settings()
 
 <div class="wrap">
 <h2>Boldy options panel</h2>
-    
+
 <form method="post" action="">
-    
+
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>Boldy documentation</h3></legend>
     <img style="margin: 10px; float: left;" src="<?php echo bloginfo('template_directory'); ?>/images/logo.png" alt="Boldy+ logo" />
     <p>
-        This is <b>Boldy+</b>, an enhanced version of the <a href=""http://wordpress.site5.net/boldy/">Boldy theme</a>, 
+        This is <b>Boldy+</b>, an enhanced version of the <a href=""http://wordpress.site5.net/boldy/">Boldy theme</a>,
         by <a href="http://www.site5.com/">Site5</a>.<br />
-        You'll find the original documentation about Boldy's usage and configuration on the Site5 website: 
-        <a href="http://wordpress.site5.net/boldy/doc/">http://wordpress.site5.net/boldy/doc/</a>, and the 
-        Boldy+ documentation on the Google code project page: 
+        You'll find the original documentation about Boldy's usage and configuration on the Site5 website:
+        <a href="http://wordpress.site5.net/boldy/doc/">http://wordpress.site5.net/boldy/doc/</a>, and the
+        Boldy+ documentation on the Google code project page:
         <a href="http://code.google.com/p/boldy-plus/">http://code.google.com/p/boldy-plus/</a>.
     </p>
     </fieldset>
-    
+
     <!-- General settings -->
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>General Settings</h3></legend>
@@ -434,7 +435,7 @@ function boldy_settings()
                     Provide full path to your logo.
                 </p>
                 <p class="notice">
-                    Current logo: 
+                    Current logo:
                     <?php if (get_option('boldy_logo_img') != '') : ?>
                     <br />
                     <br />
@@ -451,12 +452,12 @@ function boldy_settings()
                 <input name="logo_alt" type="text" id="logo_alt" value="<?php echo get_option('boldy_logo_alt'); ?>" class="regular-text" />
             </td>
         </tr>
-        
+
          <tr valign="top">
             <th scope="row"><label for="cufon">Cufon font replacement</label></th>
             <td>
                 <select name="cufon" id="cufon">
-                    <option value="yes" <?php if(get_option('boldy_cufon') == 'yes'){?>selected="selected"<?php }?>>Yes</option>        
+                    <option value="yes" <?php if(get_option('boldy_cufon') == 'yes'){?>selected="selected"<?php }?>>Yes</option>
                     <option value="no" <?php if(get_option('boldy_cufon') == 'no'){?>selected="selected"<?php }?>>No</option>
                 </select><br />
                 <p class="notice">
@@ -473,12 +474,12 @@ function boldy_settings()
         </tr>
     </table>
     </fieldset>
-    
+
     <p class="submit">
         <input type="submit" name="Submit" class="button-primary" value="Save Changes" />
         <input type="hidden" name="boldy_settings" value="save" style="display:none;" />
     </p>
-    
+
     <!-- Social settings -->
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>Social links settings</h3></legend>
@@ -492,7 +493,7 @@ function boldy_settings()
         <tr valign="top">
             <th scope="row"><label for="latest_tweet">Display latest tweet</label></th>
             <td>
-                <select name="latest_tweet" id="latest_tweet">        
+                <select name="latest_tweet" id="latest_tweet">
                     <option value="yes" <?php if(get_option('boldy_latest_tweet') == 'yes'){?>selected="selected"<?php }?>>Yes</option>
                     <option value="no" <?php if(get_option('boldy_latest_tweet') == 'no'){?>selected="selected"<?php }?>>No</option>
                 </select>
@@ -521,12 +522,12 @@ function boldy_settings()
         </tr>
     </table>
     </fieldset>
-    
+
     <p class="submit">
         <input type="submit" name="Submit" class="button-primary" value="Save Changes" />
         <input type="hidden" name="boldy_settings" value="save" style="display:none;" />
     </p>
-    
+
     <!-- Homepage settings -->
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>Homepage settings</h3></legend>
@@ -592,7 +593,7 @@ function boldy_settings()
             <td>
                 <?php wp_dropdown_pages("name=home_box3&show_option_none=- ".__('Select')." -&selected=" .get_option('boldy_home_box3')); ?>
             </td>
-        </tr>    
+        </tr>
         <tr valign="top">
             <th scope="row"><label for="home_box3_link">Home box3 "read more" link</label></th>
             <td>
@@ -605,8 +606,8 @@ function boldy_settings()
          <tr>
             <th><label for="blurb_enable">Display homepage blurb</label></th>
             <td>
-                <select name="blurb_enable" id="blurb_enable"> 
-                    <option value="yes" <?php if(get_option('boldy_blurb_enable') == 'yes'){?>selected="selected"<?php }?>>Yes</option>        
+                <select name="blurb_enable" id="blurb_enable">
+                    <option value="yes" <?php if(get_option('boldy_blurb_enable') == 'yes'){?>selected="selected"<?php }?>>Yes</option>
                     <option value="no" <?php if(get_option('boldy_blurb_enable') == 'no'){?>selected="selected"<?php }?>>No</option>
                 </select>
                 <p class="notice">
@@ -640,16 +641,16 @@ function boldy_settings()
         </tr>
     </table>
     </fieldset>
-    
+
     <p class="submit">
         <input type="submit" name="Submit" class="button-primary" value="Save Changes" />
         <input type="hidden" name="boldy_settings" value="save" style="display:none;" />
     </p>
-    
+
     <!-- Contact settings -->
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>Contact page settings</h3></legend>
-    <table class="form-table">    
+    <table class="form-table">
         <tr>
             <td colspan="2"></td>
         </tr>
@@ -667,12 +668,12 @@ function boldy_settings()
         </tr>
     </table>
     </fieldset>
-    
+
     <p class="submit">
         <input type="submit" name="Submit" class="button-primary" value="Save Changes" />
         <input type="hidden" name="boldy_settings" value="save" style="display:none;" />
     </p>
-    
+
     <!-- Footer settings -->
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>Footer</h3></legend>
@@ -683,8 +684,8 @@ function boldy_settings()
         <tr>
             <th><label for="footer_actions">Display footer twitter &amp; quick contact section</label></th>
             <td>
-                <select name="footer_actions" id="footer_actions"> 
-                    <option value="yes" <?php if(get_option('boldy_footer_actions') == 'yes'){?>selected="selected"<?php }?>>Yes</option>        
+                <select name="footer_actions" id="footer_actions">
+                    <option value="yes" <?php if(get_option('boldy_footer_actions') == 'yes'){?>selected="selected"<?php }?>>Yes</option>
                     <option value="no" <?php if(get_option('boldy_footer_actions') == 'no'){?>selected="selected"<?php }?>>No</option>
                 </select>
             </td>
@@ -692,8 +693,8 @@ function boldy_settings()
         <tr>
             <th><label for="actions_hide">Default section visibility</label></th>
             <td>
-                <select name="actions_hide" id="actions_hide"> 
-                    <option value="visible" <?php if(get_option('boldy_actions_hide') == 'visible'){?>selected="selected"<?php }?>>Visible</option>        
+                <select name="actions_hide" id="actions_hide">
+                    <option value="visible" <?php if(get_option('boldy_actions_hide') == 'visible'){?>selected="selected"<?php }?>>Visible</option>
                     <option value="hidden" <?php if(get_option('boldy_actions_hide') == 'hidden'){?>selected="selected"<?php }?>>Hidden</option>
                 </select>
             </td>
@@ -712,12 +713,12 @@ function boldy_settings()
         </tr>
     </table>
     </fieldset>
-    
+
     <p class="submit">
         <input type="submit" name="Submit" class="button-primary" value="Save Changes" />
         <input type="hidden" name="boldy_settings" value="save" style="display:none;" />
     </p>
-    
+
     <!-- SEO settings -->
     <fieldset style="border:1px solid #ddd; padding-bottom:20px; margin-top:20px;">
     <legend><h3>SEO</h3></legend>
@@ -745,20 +746,20 @@ function boldy_settings()
         </tr>
     </table>
     </fieldset>
-    
+
     <p class="submit">
         <input type="submit" name="Submit" class="button-primary" value="Save Changes" />
         <input type="hidden" name="boldy_settings" value="save" style="display:none;" />
     </p>
-    
+
 </form>
 </div>
-<?php 
+<?php
 }
 
 
 /*******************************
-  CONTACT FORM 
+  CONTACT FORM
 ********************************/
 
  function hexstr($hexstr) {
